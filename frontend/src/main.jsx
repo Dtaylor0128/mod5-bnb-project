cfc4b55f7b61ac0fc674f5388e9c30af49e0c211
dtaylor0128 <dtaylor0128@gmail.com>
2025-03-12 23:02:20 -0700

    initial setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}// creates a variable to access store, expose it to window but only in development
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
