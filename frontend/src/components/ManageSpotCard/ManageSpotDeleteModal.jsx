0368ab43dbe4cd3ad85c2d7545fd4c8f48dd220e
dtaylor0128 <dtaylor0128@gmail.com>
2025-06-30 16:59:13 -0700

    final deplayment changes

--- frontend/src/components/ManageSpotCard/ManageSpotDeleteModal.jsx
+++ frontend/src/components/ManageSpotCard/ManageSpotDeleteModal.jsx
@@ -1,28 +1,11 @@
 import { useDispatch } from "react-redux"; //, useSelector
-// import { useNavigate } from "react-router-dom";
+
 import { useModal } from '../../context/Modal';
 import { deleteSpotThunk } from "../../store/spots";
-// import { Navigate } from "react-router-dom";
-//import * as reviewActions from '../../store/reviews';
-//import * as spotActions from '../../store/spots';
 
-// const ManageSpotDeleteModal = ({ spotId }) => {
-//     const dispatch = useDispatch()
-//     const navigate = useNavigate();
-//     const { closeModal } = useModal();
-
-
-//     const handleClickDelete = async (e) => {
-//         e.preventDefault();
-
-//         await dispatch(deleteSpotThunk(spotId));
-//         onclose();
-//         navigate("/spots/current"); // redirect to mange spots page
-//         // await dispatch(spotActions.readSpotThunk(spotId)).then(closeModal())
-//     };
 const ManageSpotDeleteModal = ({ spotId, onClose, onDeleteSuccess }) => {
     const dispatch = useDispatch();
-
+    const { closeModal } = useModal();
     const handleClickDelete = async (e) => {
         e.preventDefault();
         try {
